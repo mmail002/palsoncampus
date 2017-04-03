a1d595afba53043eca5f0e48ebf71e079daa9abe
Jayant Arora <jaistudy1996@gmail.com>
2017-04-03 19:44:19 -0400

    Update StructuredProperty for pastCampus and update comments

--- data_model.py
+++ data_model.py
@@ -11,27 +11,35 @@
 	# 3. Make StructuredProperty for past campuses.
 	# 4. Limit about to 255 characters. -- ComputedProperty maybe?
 	# 5. Define StructuredProperty for uploadedPictures.
+	# 7. Hometown API integration
+
+### DONE
+	# 6. StructuredProprty for pastCampus
+
 
 from google.appengine.ext import ndb
+
+class PastCampus(ndb.Model):
+	pastCampus = ndb.StringProperty() # this will be ID in string format.
 
 class Profile(ndb.Model):
 	profileID = ndb.KeyProperty(required = True)
 	email = ndb.StringProperty(required = True)
 	nickName = ndb.StringProperty()
 
-	password = ndb.StringProperty(required = True) # Needs to have salt added to it and the converted to hash64
-	salt = ndb.StringProperty() # this will generate a random value that will be appended to the password to make it strong. 
+	password = ndb.StringProperty(required = True) # TODO Needs to have salt added to it and the converted to hash64
+	salt = ndb.StringProperty() # TODO this will generate a random value that will be appended to the password to make it strong. 
 
 	firstName = ndb.StringProperty(required = True)
 	lastName = ndb.StringProperty(required = True)
 	birthDate = ndb.DateProperty(required = True)
 
 	status = ndb.BooleanProperty(required = True) # Shows inactive or active -- false or true respectively
-	public = ndb.BooleanProperty()
+	public = ndb.BooleanProperty() # True if profile is public and false is not.
 
 	campus = ndb.StringProperty(required = True) # This will store key from the collegeScorecard db and will referenced as such. 
-	pastCampus = ndb.StructuredProperty()
-	howetown = ndb.StringProperty() # TEMPORARY until we have an API
+	pastCampus = ndb.StructuredProperty(PastCampus, repeated = True)
+	howetown = ndb.StringProperty() # TODO TEMPORARY until we have an API
 	about = ndb.StringProperty() # Something about yourself - Limit for 255 chr still needed.
 	profilePicture = ndb.StringProperty() # this will store the url for the profile picture.
 
