1b5a47f18d33bd7e74d419bbf916cb0c23d1daf3
kobri027 <kobri027@plattsburgh.edu>
2017-04-11 10:19:38 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,3 @@
 function EventPrefs(){
-	window.open("Event.html", "", 'top=100,left=500,width=900,height=500,scrollbars=yes'); 
+	window.open("Event.html", "", 'top=250,left=500,width=900,height=500,scrollbars=yes'); 
 }
