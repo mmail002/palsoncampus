9815181c1d6d2021c6fab2756492f7d63b4bfbbc
Jayant Arora <jaistudy1996@gmail.com>
2017-04-14 15:46:02 -0400

    set up homepage to use collegescoreacard api to fetch names

--- static/college.js
+++ static/college.js
@@ -1,13 +1,34 @@
 
-function getCollegeInfo(school_name){
+/* 
+ * @author: Jayant Arora <jaistudy1996@gmail.com>
+ * @description: This file runs scripts to fetch college names from collegescorecard api
+ * @date: Apr 14, 2017.
+ */
+
+
+var getCollegeInfo = function(school_name){
 	var xhr = new XMLHttpRequest();
-	xhr.responsetype = 'json';
+	
 	// api_key need to be changed to the one used for the project.
-	var api_key = api_key="iISiVMY34WrNnYh3reQGYUshNnNKu3QyuprdcKh1";
-	var url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=" + api_key +  "&school.name=" + school_name + "&_fields=id,school.name,school.state,school.city,school.zip"
+	var api_key = "iISiVMY34WrNnYh3reQGYUshNnNKu3QyuprdcKh1";
+	var url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=" + api_key +  "&school.name=" + school_name + "&_fields=id,school.name,school.state,school.city,school.zip";
 	xhr.open('GET', url, true);
+	xhr.responseType = 'json';
+	var dataListDiv = document.getElementById('campus_names_data_div');
 	xhr.onreadystatechange = function(){
-		console.log(xhr.response);
+		if(this.readyState == 4 && this.status == 200){
+			if(dataListDiv.childNodes.length > 0){
+				dataListDiv.removeChild(dataListDiv.firstChild);
+			}
+			var dataList = document.createElement('datalist');
+			dataList.id = 'campus_names_data';
+			for(var i=0; i<xhr.response.results.length; i++){
+				var option = document.createElement('option');
+				option.value = xhr.response.results[i]["school.name"];
+				dataList.appendChild(option);
+			}
+			dataListDiv.appendChild(dataList);
+		}
 	};
 	xhr.send();
 }
