cee251e0d77056630b8db57cf706402153de1949
Jesse <Jesse@Jesses-MacBook-Pro.local>
2017-04-25 13:33:34 -0400

    Google maps API now works when you hit enter on location field

--- static/button.js
+++
@@ -1,3 +0,0 @@
-function EventPrefs(){
-	window.open("/templates/Event.html", "", 'top=100,left=225,width=900,height=500,scrollbars=yes'); 
-}
--- static/events.js
+++ static/events.js
@@ -1,13 +1,13 @@
 function codeAddress() { /*Using Google maps API, gets lat and long and will load those in the loadWeather 
     func, the lat and long are returned by entering a location in serach bar and hitting enter */
-    var address = document.getElementById("address").value; // Looks at input box with id address
+    var address = document.getElementById("EventLocation").value; // Looks at input box with id address
+    alert(address);
     var geocoder = new google.maps.Geocoder(); //Call to Google maps API
 
     geocoder.geocode( { 'address': address}, function(results, status) {
         var location = results[0].geometry.location; // return location lat and long
-        //alert('LAT: ' + location.lat() + ' LANG: ' + location.lng()*-1);
-        loadWeather(location.lat()+','+location.lng())
-        
+        alert(location.lat() + location.lng());
+        //alert('LAT: ' + location.lat() + ' LANG: ' + location.lng()*-1);      
         var uluru = {lat: location.lat(), lng: location.lng()};
             var map = new google.maps.Map(document.getElementById('map'), {
               zoom: 4,
