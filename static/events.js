2ac20a271a2d51c715f3825572fe4bbc7e7becd5
Jesse <Jesse@Jesses-MacBook-Pro.local>
2017-05-02 11:35:34 -0400

    removed alerts from events.js

--- static/events.js
+++ static/events.js
@@ -1,13 +1,10 @@
 function codeAddress() { /*Using Google maps API, gets lat and long and will load those in the loadWeather 
     func, the lat and long are returned by entering a location in serach bar and hitting enter */
     var address = document.getElementById("EventLocation").value; // Looks at input box with id address
-    alert(address);
     var geocoder = new google.maps.Geocoder(); //Call to Google maps API
 
     geocoder.geocode( { 'address': address}, function(results, status) {
-        var location = results[0].geometry.location; // return location lat and long
-        alert(location.lat() + location.lng());
-        //alert('LAT: ' + location.lat() + ' LANG: ' + location.lng()*-1);      
+        var location = results[0].geometry.location; // return location lat and long 
         var uluru = {lat: location.lat(), lng: location.lng()};
             var map = new google.maps.Map(document.getElementById('map'), {
               zoom: 4,
